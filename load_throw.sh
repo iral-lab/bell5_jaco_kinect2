f2726c53251670bd42d28aee2bb373f338b5935c
Neil Bell <bell5@umbc.edu>
2016-02-21 11:34:34 -0500

    updated load pose

--- load_throw.sh
+++ load_throw.sh
@@ -4,4 +4,4 @@ ONE_EIGHTY=3.14159
 ./straighten_arm.sh
 
 echo Lowering
-rosrun jaco_demo joint_angle_workout.py jaco 0.5 0 -$ONE_EIGHTY $NINETY $ONE_EIGHTY -1.8
+rosrun jaco_demo joint_angle_workout.py jaco -0.5 0 -$ONE_EIGHTY $NINETY $ONE_EIGHTY -1.8
