2d5bfc8bd2f97704a483589e4eb62823581444a7
Neil Bell <bell5@umbc.edu>
2016-03-17 12:33:34 -0400

    updated with proper tab spacing

--- src/viz.h
+++ src/viz.h
@@ -24,57 +24,50 @@
 
 static const std::string OPENCV_WINDOW = "Image window";
 
-class ImageConverter
-{
-  ros::NodeHandle nh_;
-  image_transport::ImageTransport it_;
-  image_transport::Subscriber image_sub_;
-  
+class ImageConverter{
+ros::NodeHandle nh_;
+image_transport::ImageTransport it_;
+image_transport::Subscriber image_sub_;
+
 public:
-  ImageConverter()
-    : it_(nh_)
-  {
-    // Subscrive to input video feed and publish output video feed
-    image_sub_ = it_.subscribe("/kinect2/hd/image_color_rect", 1, 
-      &ImageConverter::imageCb, this);
+ImageConverter() : 
+	it_(nh_){
+		// Subscrive to input video feed and publish output video feed
+		image_sub_ = it_.subscribe("/kinect2/hd/points", 1, 
+		&ImageConverter::imageCb, this);
 
-    cv::namedWindow(OPENCV_WINDOW);
-  }
+		cv::namedWindow(OPENCV_WINDOW);
+	}
 
-  ~ImageConverter()
-  {
-    cv::destroyWindow(OPENCV_WINDOW);
-  }
+	~ImageConverter(){
+		cv::destroyWindow(OPENCV_WINDOW);
+	}
 
-  void imageCb(const sensor_msgs::ImageConstPtr& msg)
-  {
-    cv_bridge::CvImagePtr cv_ptr;
-    try
-    {
-      cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
-    }
-    catch (cv_bridge::Exception& e)
-    {
-      ROS_ERROR("cv_bridge exception: %s", e.what());
-      return;
-    }
+	void imageCb(const sensor_msgs::ImageConstPtr& msg){
+		cv_bridge::CvImagePtr cv_ptr;
+		try{
+			cv_ptr = cv_bridge::toCvCopy(msg, sensor_msgs::image_encodings::BGR8);
+		}catch (cv_bridge::Exception& e){
+			ROS_ERROR("cv_bridge exception: %s", e.what());
+			return;
+		}
 
-    // Draw an example circle on the video stream
-    if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
-      cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
+		// Draw an example circle on the video stream
+		if (cv_ptr->image.rows > 60 && cv_ptr->image.cols > 60)
+			cv::circle(cv_ptr->image, cv::Point(50, 50), 10, CV_RGB(255,0,0));
 
-    // Update GUI Window
-    cv::imshow(OPENCV_WINDOW, cv_ptr->image);
-    cv::waitKey(3);
-    
-  }
+		// Update GUI Window
+		cv::imshow(OPENCV_WINDOW, cv_ptr->image);
+		cv::waitKey(3);
+
+	}
 };
 
 
 void handle_viz(int argc, char **argv){
 	
 	ros::init(argc, argv, "image_converter");
-  ImageConverter ic;
-  ros::spin();
+	ImageConverter ic;
+	ros::spin();
 	
 }
