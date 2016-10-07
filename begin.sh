e0c1448b477ee76741e94362bd9ce1b62e341b32
Michael Neary <mneary1@umbc.edu>
2016-10-07 15:14:21 -0400

    needed scripts to start/kill

---
+++ begin.sh
@@ -0,0 +1,23 @@
+gnome-terminal -e "roscore"
+sleep 2
+gnome-terminal -e "rosrun jaco_driver jaco_arm_driver"
+sleep 2
+gnome-terminal -e "roslaunch kinect2_bridge kinect2_bridge.launch depth_method:=cpu"
+
+
+check_cmd="rostopic info /jaco_arm_driver/out/joint_state"
+
+code=1
+while [ $code -ne 0 ]
+do
+  sleep 1
+  echo Verifying JACO is broadcasting and ready
+  $check_cmd > /dev/null
+  code=$?
+done
+
+echo Found arm
+
+./init_arm.sh
+
+echo Ready
