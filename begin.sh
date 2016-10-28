510e6173d3d29568032cf666ffb40b38296474f6
Neil Bell <bell5@umbc.edu>
2016-10-28 12:14:18 -0400

    updated begin script to be able to kill/rerun the startup scripts.

--- begin.sh
+++ begin.sh
@@ -1,8 +1,8 @@
 gnome-terminal -e "roscore"
 sleep 2
-gnome-terminal -e "rosrun jaco_driver jaco_arm_driver"
+gnome-terminal -e "bash -c 'while true; do rosrun jaco_driver jaco_arm_driver; sleep 1; done'"
 sleep 2
-gnome-terminal -e "roslaunch kinect2_bridge kinect2_bridge.launch depth_method:=cpu"
+gnome-terminal -e "bash -c 'while true; do roslaunch kinect2_bridge kinect2_bridge.launch depth_method:=cpu; sleep 1; done'"
 
 
 check_cmd="rostopic info /jaco_arm_driver/out/joint_state"
