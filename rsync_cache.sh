fee82f7c7236fc71c09a9e48fc99c5ff6ac7c299
Neil Bell <bell5@umbc.edu>
2017-01-27 15:51:10 -0500

    sync the best files too

--- rsync_cache.sh
+++ rsync_cache.sh
@@ -1,5 +1,6 @@
 while true; do
 	rsync -v -e "ssh -i ~/.ssh/neilrbell_solo_20160113.pem" ec2-user@${1}:~/bell5_jaco_kinect2/caches/* caches/
+	rsync -v -e "ssh -i ~/.ssh/neilrbell_solo_20160113.pem" ec2-user@${1}:~/bell5_jaco_kinect2/best_* .
 	echo "Done, sleeping"
 	# sleep for 10 minutes
 	sleep 600
