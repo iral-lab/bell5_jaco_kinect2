24a314adccdf458417bc97280e4659895ab04a30
Neil Bell <bell5@umbc.edu>
2017-03-25 10:31:37 -0400

    set # cores as a parameter to the script

--- compile_and_run.sh
+++ compile_and_run.sh
@@ -1,7 +1,14 @@
 reset
 
+if [ -z $1 ];
+then
+	echo "must give number of cores to use"
+	exit
+fi
+
+
 PROG="attachment"
-CORES="4"
+CORES=$1
 COMP="mpicc -o $PROG attachment.c"
 RUN="mpirun --host localhost -np $CORES ./$PROG datasets/diverse_movement_skeleton.csv datasets/diverse_movement_pcl.csv"
 
