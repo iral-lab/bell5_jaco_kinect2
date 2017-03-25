692a1b4dd13db20bb4b5e271f92dddeddc7c76a7
Neil Bell <bell5@umbc.edu>
2017-03-25 08:40:55 -0400

    added two upfront checks for invalid points

--- util.h
+++ util.h
@@ -207,8 +207,41 @@ void read_and_broadcast_frames(char **argv, int *num_skeleton_frames, frame **al
 	read_frames(skeleton_handle, SKELETON, num_skeleton_frames, all_skeleton_frames);
 	printf("done reading in %i skeleton frames\n", *num_skeleton_frames);
 	
+	for(int k = 0; k < (*num_skeleton_frames); k++){
+		frame *frm = &((*all_skeleton_frames)[k]);
+		for(int j = 0; j < frm->num_points; j++){
+			if(frm->points[j].x == 0 || frm->points[j].y == 0 || frm->points[j].z == 0){
+				printf("HAS INVALID FRAME\n");
+				printf("Frame points %i\n", frm->num_points);
+				for(int i = 0; i < frm->num_points; i++){
+					printf("%f %f %f\n", frm->points[i].x, frm->points[i].y,frm->points[i].z);
+				}
+				exit(1);
+			}
+		}
+	}
+
+	
 	read_frames(pcl_handle, POINTCLOUD, num_pointcloud_frames, all_pointcloud_frames);
 	printf("done reading in %i pointcloud frames\n", *num_pointcloud_frames);
+	
+	
+	for(int k = 0; k < (*num_pointcloud_frames); k++){
+		frame *frm = &((*all_pointcloud_frames)[k]);
+		for(int j = 0; j < frm->num_points; j++){
+			if(frm->points[j].x == 0 || frm->points[j].y == 0 || frm->points[j].z == 0){
+				printf("HAS INVALID FRAME\n");
+				printf("Frame points %i\n", frm->num_points);
+				for(int i = 0; i < frm->num_points; i++){
+					printf("%f %f %f\n", frm->points[i].x, frm->points[i].y,frm->points[i].z);
+				}
+				exit(1);
+			}
+		}
+	}
+	
+	
+	
 	
 	MPI_Bcast(num_skeleton_frames, 1, MPI_INT, 0, MPI_COMM_WORLD);
 	MPI_Bcast(num_pointcloud_frames, 1, MPI_INT, 0, MPI_COMM_WORLD);
