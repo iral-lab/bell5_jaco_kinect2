4e3c7051fa505e6c7b37cd3aea86d141a1e85a16
Neil Bell <bell5@umbc.edu>
2017-09-16 06:48:21 -0700

    updated to be more robust

--- ec2_setup_gpu.sh
+++ ec2_setup_gpu.sh
@@ -3,12 +3,14 @@
 
 sudo apt-get update
 sudo apt-get upgrade -y
-sudo apt-get install -y build-essential python-pip python-dev git python-numpy swig python-dev default-jdk zip zlib1g-dev htop screen git gcc
+sudo apt-get install -y awscli build-essential python-pip python-dev git python-numpy swig python-dev default-jdk zip zlib1g-dev htop screen git gcc
 
 wget https://developer.download.nvidia.com/compute/cuda/repos/ubuntu1404/x86_64/cuda-repo-ubuntu1404_8.0.61-1_amd64.deb
 sudo dpkg -i cuda-repo-ubuntu1404_8.0.61-1_amd64.deb
 sudo apt-get update
 sudo apt-get install cuda -y
+
+sudo reboot
 
 # scp -i ~/.ssh/neilrbell_solo_20160113.pem Downloads/cudnn-8.0-linux-x64-v6.0.tgz ubuntu@34.229.153.57:~/
 tar zxf cudnn-8.0-linux-x64-v6.0.tgz
@@ -23,12 +25,17 @@ echo "export CUDA_ROOT=/usr/local/cuda" >> ~/.bashrc
 echo "export PATH=$PATH:/usr/local/cuda/bin" >> ~/.bashrc
 echo "export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64" >> .bashrc
 
+export CUDA_HOME=/usr/local/cuda
+export CUDA_ROOT=/usr/local/cuda
+export PATH=$PATH:/usr/local/cuda/bin
+export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/usr/local/cuda/lib64
+
 
 time nvidia-smi
 sudo nvidia-smi -pm 1
 sudo nvidia-smi --auto-boost-default=0
 sudo nvidia-smi -ac 2505,875
-
+time nvidia-smi
 
 
 
@@ -47,11 +54,15 @@ sudo chown ubuntu:ubuntu -R /usr/lib/python*
 sudo chown ubuntu:ubuntu -R /usr/local/lib/python*
 
 pip install --user numpy scipy matplotlib ipython jupyter pandas sympy nose pylru
+pip install -U scikit-learn
 
 sudo pip install tensorflow-gpu
 
 sudo chown ubuntu:ubuntu -R /usr/lib/python*
 sudo chown ubuntu:ubuntu -R /usr/local/lib/python*
 
+python -c "import tensorflow as tf; print tf.__version__"
+
 git clone https://github.com/iral-lab/bell5_jaco_kinect2.git
+touch ~/bell5_jaco_kinect2/.on_aws
 
