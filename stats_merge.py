e5f77a758102e13d87d4e76b3ea1522422ea03da
Neil Bell <bell5@umbc.edu>
2017-09-22 13:16:33 -0400

    tweaks to clean up output

--- stats_merge.py
+++ stats_merge.py
@@ -17,15 +17,28 @@ if "__main__" == __name__:
 		folder_parts = folder.split("_")
 		n_layers, n_nodes = [int(x) for x in folder_parts[2:]]
 		
-		run_label = str(n_layers * 10000 + n_nodes)
+		run_label = str(n_layers) + "  " + str(n_nodes)
 		while len(data) < 1:
 			data.append([])
 		if first_time:
 			data[0].append("") # empty upper-left cell
 			first_time = False
-		data[0].append(run_label)
 		
+		if True:
+			valid_suffixes = [
+				"_20", "_80", "_140", "_190",
+			]
+			valid = False
+			for suffix in valid_suffixes:
+				if folder[-1 * len(suffix):] == suffix:
+					valid = True
+					break
+			if not valid:
+				continue
+		data[0].append(run_label)
+				
 		folder = IN_DIR + folder + "/"
+
 		for file in sorted(os.listdir(folder)):
 			file = folder + file
 			print file
@@ -61,7 +74,11 @@ if "__main__" == __name__:
 		# break
 	
 	to_write = []
-	for row in data:
+	for i,row in enumerate(data):
+		# print only after final training
+		if not row[0] == "" and not row[0] % 100 == 9:
+			continue
+		
 		row = [str(x) for x in row]
 		to_write.append("\t".join(row))
 	open("merged_run_stats.tsv", "w").write("\n".join(to_write))
