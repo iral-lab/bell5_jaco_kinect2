2f6e60cf885459e2abe099563beccf73a1aefd27
Neil Bell <bell5@umbc.edu>
2017-11-01 19:34:44 -0400

    updated stats merge

--- stats_merge.py
+++ stats_merge.py
@@ -5,7 +5,7 @@ IN_DIR = "run_stats/"
 RUN_MLP, RUN_RNN = range(2)
 RUN_TYPE = RUN_RNN
 
-MLP_SUFFIXES = ["_20", "_80", "_140", "_190",]
+MLP_SUFFIXES = None #["_20", "_80", "_140", "_190",]
 RNN_SUFFIXES = ["_20", "_60", "_100", "_140", "_180"]
 
 if "__main__" == __name__:
@@ -38,17 +38,21 @@ if "__main__" == __name__:
 		if True:
 			valid_suffixes = MLP_SUFFIXES if RUN_TYPE == RUN_MLP else RNN_SUFFIXES
 			valid = False
-			for suffix in valid_suffixes:
-				if folder[-1 * len(suffix):] == suffix:
-					valid = True
-					break
+			if not valid_suffixes:
+				valid = True
+			else:
+				for suffix in valid_suffixes:
+					if folder[-1 * len(suffix):] == suffix:
+						valid = True
+						break
 			if not valid:
 				continue
-		data[0].append(run_label)
 				
 		folder = IN_DIR + folder + "/"
 
+		
 		for file in sorted(os.listdir(folder)):
+			has_lines = False
 			file = folder + file
 			print file
 			lines = open(file, 'r').readlines()
@@ -57,9 +61,14 @@ if "__main__" == __name__:
 			
 			parts = []
 			for line in lines:
+				while '  ' in line:
+					line = line.replace('  ',' ')
+				line = line.replace(' ',"\t")
 				items = line.strip().split("\t")
 				items = [float(x) for x in items]
 				parts.append(items)
+			# print len(parts)
+			# print parts[0]
 			
 			if not labels:
 				labels = []
@@ -72,8 +81,12 @@ if "__main__" == __name__:
 					data[i+1].append(label)
 			
 			for i,row in enumerate(parts):
+				has_lines = True
 				data[i+1].append(row[train_cost_index])
-			
+			if has_lines:
+				data[0].append(run_label)
+			else:
+				print "\tNo data!"
 	to_write = []
 	for i,row in enumerate(data):
 		# print only after final training
